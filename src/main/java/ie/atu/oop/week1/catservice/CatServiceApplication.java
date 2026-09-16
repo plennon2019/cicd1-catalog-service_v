d3012299c4c621dc693d62df6a1b3fc83a0215cd
Paul Lennon <Paul.Lennon@atu.ie>
2026-09-16 14:24:02 +0100

    Base layer for Catalog Service

---
+++ src/main/java/ie/atu/oop/week1/catservice/CatServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.oop.week1.catservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oop/week1/catservice/CatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oop.week1.catservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
